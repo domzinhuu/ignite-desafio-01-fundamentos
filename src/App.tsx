f8ba7bb7418a61bfa062e083cfaf1c63ee0126f9
Maique Rosa <maique.rosa@gmail.com>
2023-05-20 22:27:53 -0300

    feat: implementando a funcionalidade de salvar as tasks (no componente pai)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 import { CreateTask } from "./components/CreateTask";
 import { Header } from "./components/Header";
 
 export function App() {
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  function onSave(task: string): void {
+    setTasks([...tasks, task]);
+  }
+
   return (
     <div className={styles.app}>
       <Header />
       <main className={styles.content}>
-        <CreateTask />
+        <CreateTask onSave={onSave} />
+
+        {/* TODO: adicionar aqui a lista de tarefas */}
       </main>
     </div>
   );
--- src/components/CreateTask.tsx
+++ src/components/CreateTask.tsx
@@ -1,11 +1,44 @@
 import { PlusCircle } from "phosphor-react";
 import styles from "./CreateTask.module.css";
+import { ChangeEvent, FormEvent, InvalidEvent, useState } from "react";
 
-export function CreateTask() {
+interface CreateTaskProps {
+  onSave: (task: string) => void;
+}
+
+export function CreateTask({ onSave }: CreateTaskProps) {
+  const [task, setTask] = useState("");
+
+  function handleSubmitTask(el: FormEvent): void {
+    el.preventDefault();
+    onSave(task);
+    setTask("");
+
+  }
+
+  function handleTaskChange(el: ChangeEvent<HTMLInputElement>) {
+    el.target.setCustomValidity("");
+    setTask(el.target.value);
+  }
+
+  function handleInvalidTask(el: InvalidEvent<HTMLInputElement>) {
+    console.log("Invalid");
+    el.target.setCustomValidity("É preciso informar a descrição da sua task!");
+  }
+
+  const isTaskEmpty = task.length === 0;
+
   return (
-    <form className={styles.createTask}>
-      <input type="text" placeholder="Adicione uma nova tarefa" />
-      <button type="submit">
+    <form onSubmit={handleSubmitTask} className={styles.createTask}>
+      <input
+        value={task}
+        onChange={handleTaskChange}
+        onInvalid={handleInvalidTask}
+        required
+        type="text"
+        placeholder="Adicione uma nova tarefa"
+      />
+      <button disabled={isTaskEmpty} type="submit">
         Criar <PlusCircle size={16} />
       </button>
     </form>
