a118529762ec9938312e16ade8c039c4dd7ff08b
Maique Rosa <maique.rosa@gmail.com>
2023-05-20 21:57:09 -0300

    feat: adicionando html e css do component createTask

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import styles from "./App.module.css";
+import { CreateTask } from "./components/CreateTask";
 import { Header } from "./components/Header";
 
 export function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <main className={styles.content}>
+        <CreateTask />
+      </main>
     </div>
   );
 }
---
+++ src/components/CreateTask.tsx
@@ -0,0 +1,13 @@
+import { PlusCircle } from "phosphor-react";
+import styles from "./CreateTask.module.css";
+
+export function CreateTask() {
+  return (
+    <form className={styles.createTask}>
+      <input type="text" placeholder="Adicione uma nova tarefa" />
+      <button type="submit">
+        Criar <PlusCircle size={16} />
+      </button>
+    </form>
+  );
+}
